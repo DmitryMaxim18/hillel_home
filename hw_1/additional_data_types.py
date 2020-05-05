aaa03e3edea300f38503bc13e4700efcc3abf743
Max <dmitro.maximov@gmail.com>
2020-05-06 02:03:17 +0300

    add first home work

---
+++ hw_1/additional_data_types.py
@@ -0,0 +1,75 @@
+# exercise_1
+
+# number = input('enter a number')
+# print(int(number, 2))
+
+# exercise_2
+
+# a = int(input('enter a number'))
+# b = int(input('enter a number'))
+# c = int(input('enter a number'))
+# formula = (input('enter formula'))
+# print(eval(formula))
+
+# exercise_3
+
+# number = int(input('enter a number'))
+# print(
+#     bin(number),
+#     oct(number),
+#     hex(number)
+# )
+
+# exercise_4
+
+# number_one = input('enter a number in ternary system')
+# number_two = input('enter a number in pentameric system')
+# number_three = input('enter a number in septenary system')
+# print(
+#     int(number_one, 3),
+#     int(number_two, 5),
+#     int(number_three, 7)
+# )
+
+# exercise_5
+
+# data = input('enter something')
+# print(hash(data))
+
+# exercise_6
+
+# number = input('enter your number')
+# print(int(len(number)))
+
+# exercise_7
+
+# number = float(input('enter your number'))
+# print(
+#     round(number, 0),
+#     round(number, 2),
+#     round(number, 5)
+# )
+
+# exercise_8
+
+# symbol_code = int(input('enter code'))
+# print(
+#      chr(symbol_code-1),
+#      chr(symbol_code),
+#      chr(symbol_code+1)
+#
+#  )
+
+# exercise_9
+
+# symbol = (input('enter symbol'))
+# symbol_code = (ord(symbol))
+# print(len(str(symbol_code**5)))
+
+
+
+
+
+
+
+
---
+++ hw_1/data_types.py
@@ -0,0 +1,74 @@
+# exercise_1
+
+# user_name = input('What is your name?')
+# user_age = input('How old are you?')
+# print('Hello ' + user_name + ' your age is ' + user_age)
+
+# exercise_2
+# number = int(input('Enter you number'))
+# print('your number after calculating the power 132 is ' + str(number**132) +
+#       ' the remainder of dividing by 3 is ' + str((number**132) % 3))
+
+# exercise_3
+
+# first_number = int(input('enter first number'))
+# second_number = int(input('enter second number'))
+# print(
+#         'sum of numbers is ' + str(first_number+second_number) + '\n'
+#         'difference of numbers is ' + str(first_number-second_number) + '\n'
+#         'dividing of numbers is ' + str(first_number/second_number) + '\n'
+#         'multiplication of numbers is ' + str(first_number*second_number) + '\n'
+#         'powering of numbers is ' + str(first_number**second_number) + '\n'
+# )
+
+# exercise_4
+
+# first_number = int(input('enter first number'))
+# second_number = int(input('enter second number'))
+# third_number = int(input('enter third number'))
+# formula = 2*first_number-(8*second_number/(first_number-second_number+third_number))
+# print('result' + str(formula))
+
+# exercise_5
+
+# string = input('type any string')
+# number = int(input('enter any number'))
+# print(string*number)
+
+# exercise_6
+
+# first_number = 125
+# second_number = 437
+# print(
+#     'the remainder of dividing first_number by 2 is ' + str(first_number % 2) + '\n'
+#     'the remainder of dividing first_number by 3 is ' + str(first_number % 3) + '\n'
+#     'the remainder of dividing first_number by 10 is ' + str(first_number % 10) + '\n'
+#     'the remainder of dividing first_number by 22 is ' + str(first_number % 22) + '\n'
+#     'the remainder of dividing second_number by 22 is ' + str(second_number % 22) + '\n'
+#     'the remainder of dividing second_number by 10 is ' + str(second_number % 10) + '\n'
+#     'the remainder of dividing second_number by 3 is ' + str(second_number % 3) + '\n'
+#     'the remainder of dividing second_number by 2 is ' + str(second_number % 2) + '\n'
+# )
+
+# exercise_7
+
+# first_number = int(input('enter first number'))
+# second_number = int(input('enter second number'))
+# print(first_number//second_number)
+
+# exercise_8
+
+# first_string = input('type first string')
+# second_string = input('type second string')
+# third_string = input('type third string')
+# total_string = first_string + ' ' + second_string + ' ' + third_string
+# print(total_string)
+
+# exercise_9
+
+# first = 15
+# second = 43
+# new_first = second
+# second = first
+# first = new_first
+# print(first, second)
